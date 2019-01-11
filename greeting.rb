1da4c66ebd663619e6e16ce66ab29906738902cb
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-01-11 01:19:12 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -5,7 +5,14 @@ def khan_family
         ["1.1.1: Ubaid Tariq", "1.1.2: Junaid Tariq", "1.1.3: Uzzam Tariq", "1.1.4: Aalyan Tariq"], 
       "1.2: Nargis Siddique", ["1.2.1: Umer Arif", "1.2.2: Huma Arif", "1.2.3: Usman Arif"], 
       "1.3: Nagina Siddique", ["1.3.1: Hassan Sheraz", "1.3.2: Hanan Sheraz"]], 
-    "2: SARDAR ANWAR HUSSAIN KHAN", ["2.1: Amir Anwar", ["2.1.1: Palwasha Amir", "2.1.2: Shazain Amir", "2.1.3: Zayan Amir"], "2.2: Yasir Anwar", ["2.2.1: Ammar Yasir", "2.2.2: Noria Yasir"], "2.3: Nasir Anwar",["2.3.1: OnHold", ["2.3.1.1: TooLate"]]]]
+    "2: SARDAR ANWAR HUSSAIN KHAN", 
+      ["2.1: Amir Anwar", 
+        ["2.1.1: Palwasha Amir", "2.1.2: Shazain Amir", "2.1.3: Zayan Amir"], 
+        "2.2: Yasir Anwar", 
+          ["2.2.1: Ammar Yasir", "2.2.2: Noria Yasir"],
+        "2.3: Nasir Anwar",
+          ["2.3.1: OnHold", 
+            ["2.3.1.1: TooLate"]]]]
     ]
 
 khan_family.each do |khan_array|
